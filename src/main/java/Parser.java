aef1c2cd4588b2d916924fe77d806d57486c522f
Dan Shadrin (xdx505) <shadrin.smr@gmail.com>
2021-01-31 11:28:10 +0400

    Refactor parse method

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,11 +1,12 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Parser {
     private final String path;
@@ -17,28 +18,60 @@ public class Parser {
     public void parse() {
         try {
             File file = new File(path);
-            Document document = Jsoup.parse(file, "UTF-8");
-            printResult(wordCount(wordsExtractor(document)));
+            Document page = Jsoup.parse(file, "UTF-8");
+            Element bodyFrame = page.body();
+
+            ArrayList<TextNode> textNodeList = getAllTextNodes(bodyFrame);
+            ArrayList<String> linesFromBlocks = injectLines(textNodeList);
+            ArrayList<String> textFromLines = wordsExtractor(linesFromBlocks);
+            printResult(wordCount(textFromLines));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private String[] wordsExtractor(Document doc) {
-        doc.getElementsByTag("pre").remove();
-        doc.getElementsByTag("img").remove();
-        String text = doc.body().text().toUpperCase().replaceAll("[^A-ZА-Я]", " ");
-        return text.split(" ");
+    private ArrayList<TextNode> getAllTextNodes(Node element) {
+        ArrayList<TextNode> arrayList = new ArrayList<>();
+        for (Node node : element.childNodes()) {
+            if (node instanceof TextNode && !((TextNode) node).isBlank()) {
+                arrayList.add((TextNode) node);
+            } else {
+                arrayList.addAll(getAllTextNodes(node));
+            }
+        }
+        //TODO if arrayList is null
+        return arrayList;
     }
 
-    private HashMap<String, Integer> wordCount(String[] words) {
-        ArrayList<String> wordsList = new ArrayList(Arrays.asList(words));
+    private ArrayList<String> injectLines(ArrayList<TextNode> textNodes) {
+        ArrayList arrayList = new ArrayList<String>();
+
+        if (!textNodes.isEmpty())
+            for (int i = 0; i < textNodes.size(); i++) {
+                arrayList.add(textNodes.get(i).text().toUpperCase().trim().replaceAll("[^A-ZА-Я]", " "));
+            }
+
+        return arrayList;
+    }
+
+    private ArrayList<String> wordsExtractor(ArrayList<String> list) {
+        ArrayList<String> wordsList = new ArrayList<>();
+        for (String line : list) {
+            if (line.contains(" ")) {
+                String[] wordsFromLine = line.split(" ");
+                wordsList.addAll(Arrays.asList(wordsFromLine));
+            }
+        }
+        return wordsList;
+    }
+
+    private HashMap<String, Integer> wordCount(ArrayList<String> words) {
+        List<String> wordsList = words;
         HashMap<String, Integer> map = new HashMap();
 
         for (int i = 0; i < wordsList.size(); i++) {
             String firstWord = wordsList.get(i);
             int count = 1;
-
             if (!firstWord.equals("")) {
                 for (int j = 1; j < wordsList.size(); j++) {
                     String secondWord = wordsList.get(j);
